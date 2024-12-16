20f714aa5d6dcf29678712f469d087adee42de84
bearcattt <ewigglet8@gmail.com>
2024-12-15 21:55:03 -0500

    fix

--- src/utils/protectedRouteHandler.ts
+++ src/utils/protectedRouteHandler.ts
@@ -1,42 +1,44 @@
-import sha256 from 'crypto-js/sha256'
-import siteConfig from '../../config/site.config'
+import sha256 from 'crypto-js/sha256';
+import siteConfig from '../../config/site.config';
 
 // Hash password token with SHA256
 function encryptToken(token: string): string {
-  return sha256(token).toString()
+  return sha256(token).toString();
 }
 
 // Fetch stored token from localStorage and encrypt with SHA256
 export function getStoredToken(path: string): string | null {
   const storedToken =
-    typeof window !== 'undefined' ? JSON.parse(localStorage.getItem(matchProtectedRoute(path)) as string) : ''
-  return storedToken ? encryptToken(storedToken) : null
+    typeof window !== 'undefined'
+      ? JSON.parse(localStorage.getItem(matchProtectedRoute(path)) as string)
+      : '';
+  return storedToken ? encryptToken(storedToken) : null;
 }
 
 /**
- * Compares the hash of .password and od-protected-token header
+ * Compares the hash of .password.txt and od-protected-token header
  * @param odTokenHeader od-protected-token header (sha256 hashed token)
- * @param dotPassword non-hashed .password file
+ * @param dotPassword non-hashed .password.txt file content
  * @returns whether the two hashes are the same
  */
 export function compareHashedToken({
   odTokenHeader,
   dotPassword,
 }: {
-  odTokenHeader: string
-  dotPassword: string
+  odTokenHeader: string;
+  dotPassword: string;
 }): boolean {
-  return encryptToken(dotPassword.trim()) === odTokenHeader
+  return encryptToken(dotPassword.trim()) === odTokenHeader;
 }
+
 /**
  * Match the specified route against a list of predefined routes
  * @param route directory path
  * @returns whether the directory is protected
  */
-
 export function matchProtectedRoute(route: string): string {
-  const protectedRoutes: string[] = siteConfig.protectedRoutes
-  let authTokenPath = ''
+  const protectedRoutes: string[] = siteConfig.protectedRoutes;
+  let authTokenPath = '';
 
   for (const r of protectedRoutes) {
     // protected route array could be empty
@@ -49,10 +51,10 @@ export function matchProtectedRoute(route: string): string {
             .join('/')
         )
       ) {
-        authTokenPath = r
-        break
+        authTokenPath = r;
+        break;
       }
     }
   }
-  return authTokenPath
+  return authTokenPath;
 }
